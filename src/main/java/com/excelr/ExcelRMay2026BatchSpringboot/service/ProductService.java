83742b9c06f946bb7945b2eaa24ab88274c47eb8
uysolanki <uysolanki@gmail.com>
2026-07-13 17:49:41 +0530

    product crud

--- src/main/java/com/excelr/ExcelRMay2026BatchSpringboot/service/ProductService.java
+++ src/main/java/com/excelr/ExcelRMay2026BatchSpringboot/service/ProductService.java
@@ -38,4 +38,29 @@ public class ProductService {
 		throw new ProductNotFoundException("Product with ID " +prodid + " does not exist");
 	}
 
+	public void deleteProduct(int prodid) {
+		if(productRepository.existsById(prodid))
+		{
+			productRepository.deleteById(prodid);
+		}
+		
+		throw new ProductNotFoundException("Product with ID " +prodid + " does not exist");
+		
+	}
+
+	public Product updateProduct(int prodid, Product updatedProduct) {
+		if(productRepository.existsById(prodid))
+		{
+			Product productFromDB=getSingleProduct(prodid);
+			productFromDB.setCategory(updatedProduct.getCategory());
+			productFromDB.setDescription(updatedProduct.getDescription());
+			productFromDB.setImage(updatedProduct.getImage());
+			productFromDB.setPrice(updatedProduct.getPrice());
+			productFromDB.setTitle(updatedProduct.getTitle());
+			return productRepository.save(productFromDB);
+		}
+		
+		throw new ProductNotFoundException("Product with ID " +prodid + " does not exist");
+	}
+
 }
