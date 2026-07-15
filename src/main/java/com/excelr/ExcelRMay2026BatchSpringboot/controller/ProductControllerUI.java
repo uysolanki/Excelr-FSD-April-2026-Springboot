d409613447f029574b9dc60fcf1449756eea28f9
uysolanki <uysolanki@gmail.com>
2026-07-15 17:47:49 +0530

    upload image

--- src/main/java/com/excelr/ExcelRMay2026BatchSpringboot/controller/ProductControllerUI.java
+++ src/main/java/com/excelr/ExcelRMay2026BatchSpringboot/controller/ProductControllerUI.java
@@ -1,5 +1,9 @@
 package com.excelr.ExcelRMay2026BatchSpringboot.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.excelr.ExcelRMay2026BatchSpringboot.entity.Product;
 import com.excelr.ExcelRMay2026BatchSpringboot.service.ProductService;
@@ -35,9 +41,39 @@ public class ProductControllerUI {
 	return "add-new-product-form";
 	}
 	
+	@RequestMapping("/add-new-product-form2")
+	public String addNewProduct1(Model model)
+	{
+	Product p1=new Product();
+	model.addAttribute("product",p1);
+	return "add-new-product-form2";
+	}
+	
 	@PostMapping("/addSingleProduct")
 	public String saveProduct(@ModelAttribute Product product)
 	{
+		productService.saveProduct(product);
+		return "redirect:/show-all-products";
+	}
+	
+	
+	@PostMapping("/addSingleProduct1")
+	public String saveProduct1(@ModelAttribute Product product, @RequestParam("imageFile") MultipartFile file) throws IOException
+	{
+		
+		if (!file.isEmpty()) {
+
+	        String uploadDir = "uploads/";
+	        Files.createDirectories(Paths.get(uploadDir));
+
+	        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+	        Path filePath = Paths.get(uploadDir, fileName);
+
+	        Files.write(filePath, file.getBytes());
+
+	        // Save only filename or relative path
+	        product.setImage("http://localhost:8090/uploads/" + fileName);
+	    }
 		productService.saveProduct(product);
 		return "redirect:/show-all-products";
 	}
